77d14f1312d851558b117baf17b6ca5db7b91186
Josue Abreu <josue.abreu@gmail.com>
2017-07-02 12:28:51 +0100

    Remove path-based dependencies from Gemfile
    We currently don't support path-based dependencies, so, we
    remove them from the Gemfile entirely.
    
    We'll resolve the Gemfile without a line (it'll be removed)
    from the file, which is not ideal but it'll at
    least allow us to resolve what needs to update as opposed
    to not resolving anything at all.

--- lib/bump/file_fetchers/ruby/bundler.rb
+++ lib/bump/file_fetchers/ruby/bundler.rb
@@ -8,6 +8,21 @@ module Bump
         def self.required_files
           %w(Gemfile Gemfile.lock)
         end
+
+        # We currently don't support path-based dependencies, so, we
+        # remove them from the Gemfile entirely.
+        #
+        # We'll resolve the Gemfile without a line (it'll be removed)
+        # from the file, which is not ideal but it'll at
+        # least allow us to resolve what needs to update as opposed
+        # to not resolving anything at all.
+        def files
+          super do |file_content|
+            file_content.lines.reject do |line|
+              line.include?("path")
+            end.join
+          end
+        end
       end
     end
   end
