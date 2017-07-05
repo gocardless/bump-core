c9c8d01dfcacbb383d26436c2b8c9667be615a52
Ernesto Jiménez <ernesto@gocardless.com>
2017-07-06 00:06:39 +0100

    Allow Gemfiles with path dependencies
    This is a very quick fix to allow processing of Gemfiles with path
    dependencies that can be resolved within the same repo.
    
    An example that would fail in the past and work with these changes:
    
    ```
    gem "somegem", path: "plugins/somegem"
    ```

--- lib/bump/file_fetchers/ruby/bundler.rb
+++ lib/bump/file_fetchers/ruby/bundler.rb
@@ -8,6 +8,27 @@ module Bump
         def self.required_files
           %w(Gemfile Gemfile.lock)
         end
+
+        private
+
+        def extra_files
+          lockfile = ::Bundler::LockfileParser.new(gemfile_lock)
+          path_specs = lockfile.specs.select do |spec|
+            spec.source.instance_of?(::Bundler::Source::Path)
+          end
+          path_specs.map do |spec|
+            dir, base = spec.source.path.split
+            file = File.join(dir, base, "#{base}.gemspec")
+            fetch_file_from_github(file)
+          end
+        end
+
+        def gemfile_lock
+          gemfile_lock = required_files.find do |file|
+            file.name == "Gemfile.lock"
+          end
+          gemfile_lock.content
+        end
       end
     end
   end
--- spec/bump/file_fetchers/ruby/bundler_spec.rb
+++ spec/bump/file_fetchers/ruby/bundler_spec.rb
@@ -4,4 +4,42 @@ require_relative "../shared_examples_for_file_fetchers"
 
 RSpec.describe Bump::FileFetchers::Ruby::Bundler do
   it_behaves_like "a dependency file fetcher"
+
+  let(:repo) { Bump::Repo.new(name: "gocardless/bump", commit: nil) }
+  let(:github_client) { Octokit::Client.new(access_token: "token") }
+  let(:file_fetcher_instance) do
+    described_class.new(repo: repo, github_client: github_client)
+  end
+
+  subject(:files) { file_fetcher_instance.files }
+  let(:url) { "https://api.github.com/repos/#{repo.name}/contents/" }
+
+  context "gemfile with path dependency" do
+    before do
+      stub_request(:get, url + "Gemfile").
+        to_return(status: 200,
+                  body: fixture(
+                    "github", "gemfile_with_path_content.json"
+                  ),
+                  headers: { "content-type" => "application/json" })
+      stub_request(:get, url + "Gemfile.lock").
+        to_return(status: 200,
+                  body: fixture(
+                    "github", "gemfile_lock_with_path_content.json"
+                  ),
+                  headers: { "content-type" => "application/json" })
+
+      stub_request(:get, url + "plugins/bump-core/bump-core.gemspec").
+        to_return(status: 200,
+                  body: fixture(
+                    "github", "gemspec_content.json"
+                  ),
+                  headers: { "content-type" => "application/json" })
+    end
+
+    it "fetches gemspec from path dependency" do
+      expect(files.map(&:name)).
+        to include("plugins/bump-core/bump-core.gemspec")
+    end
+  end
 end
